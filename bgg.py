61d041628dad3283cb8f94cbf4d0c5ba792c1a49
Rejtő István <reistvan@gmail.com>
2019-05-12 21:50:37 +0200

    Add threading to handle multiple applications

--- bgg.py
+++ bgg.py
@@ -1,48 +1,36 @@
-import urllib.request
 import time
 import logging
 from bgg.data_fetcher import DataFetcher
 from bgg.data_converter import DataConverter
 from bgg.data_holder import DataHolder
 
+import threading
 
 # logging.basicConfig(level=logging.DEBUG,
 #                     format='[%(levelname)s] (%(threadName)s) %(message)s')
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)s [%(levelname)s] %(funcName)s - %(message)s'
+    format='%(asctime)s [%(levelname)s] (%(threadName)s) %(funcName)s - %(message)s'
 )
 
 
-def worker():
-    logging.debug("Starting")
-    time.sleep(2)
-    logging.debug("Ending")
+class MyThread(threading.Thread):
+    def run(self) -> None:
+        df = DataFetcher()
+        dh = DataHolder()
+        dc = DataConverter(data_fetcher=df, data_holder=dh)
+        dc.convert()
 
 
-def my_service():
-    logging.debug("Starting")
-    time.sleep(3)
-    logging.debug("Ending")
+logging.info("START")
 
+thr = MyThread()
+thr.start()
 
-# logging.info("START")
-# threads = [threading.Thread(name='my_service', target=my_service),
-#            threading.Thread(name='worker', target=worker),
-#            threading.Thread(target=worker)]
-#
-# logging.info("Starting threads")
-# for thread in threads:
-#     thread.start()
-#
-# logging.info("Waiting for threads to finish")
-# for thread in threads:
-#     thread.join()
-#
-# logging.info("EXIT")
+main_thread = threading.currentThread()
+for t in threading.enumerate():
+    if t is not main_thread:
+        t.join()
 
-df = DataFetcher()
-dh = DataHolder()
-dc = DataConverter(data_fetcher=df, data_holder=dh)
-dc.convert()
+logging.info("EXIT")
