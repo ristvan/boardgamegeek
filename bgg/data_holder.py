89907fc1ab2aab6a33c9de77085fc6fd9d901111
eistrej <reistvan@gmail.com>
2019-05-14 15:59:59 +0200

    Add missing code to DataHolder class

--- bgg/data_holder.py
+++ bgg/data_holder.py
@@ -2,11 +2,17 @@ import logging
 
 
 class DataHolder:
+    def __init__(self, data_storage_factory=None):
+        self._data_storage = data_storage_factory.create_communication_channel()
+
     def add_user(self, user):
-        logging.info("user: {}".format(str(user)))
+        logging.info("User: {}".format(str(user)))
+        self._data_storage.send(str(user))
 
     def add_player(self, player):
-        logging.info("player: {}".format(str(player)))
+        logging.info("Player: {}".format(str(player)))
+        self._data_storage.send(str(player))
 
     def add_game(self, game):
-        logging.info("game: {}".format(str(game)))
+        logging.info("Game: {}".format(str(game)))
+        self._data_storage.send(str(game))
